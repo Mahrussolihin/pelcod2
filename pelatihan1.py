40364f04465392c3f24dd0e74477aa95ce64707d
Mahrussolihin <143859887+Mahrussolihin@users.noreply.github.com>
2023-09-02 11:00:37 +0700

    Add files via upload
    Upload File

---
+++ pelatihan1.py
@@ -0,0 +1,33 @@
+Nama_saya = "Nama saya Mahrus"
+print(Nama_saya,"bertipe", type(Nama_saya))
+Umur_saya = 19
+print(Umur_saya, "bertipe", type(Umur_saya))
+IPK = 4.00
+print(IPK, "bertipe", type(IPK))
+Menikah = False
+print(Menikah, "bertipe", type(Menikah))
+Matkul_yang_diambil1 = ["Alpro", "SIG"]
+print("Matkul", Matkul_yang_diambil1, "bertipe", type(Matkul_yang_diambil1))
+Matkul_yang_diambil2 = ("PTIK", "LE")
+print("Matkul lain : ", Matkul_yang_diambil2, "bertipe", type(Matkul_yang_diambil2))
+Matkul_yang_diambil3 = {"SO", "Etika Profesi"}
+print("Matkul lainnya : ", Matkul_yang_diambil3, "bertipe", type(Matkul_yang_diambil3))
+
+print()
+print(33 + 5)
+print(33 - 5)
+print(33 * 5)
+print(33 / 5)
+print(33 // 5)
+print(33 % 5)
+
+print()
+
+Nama_saya = str(input("Masukkan Nama = " ))
+print(Nama_saya,"bertipe", type(Nama_saya))
+Umur_saya = int(input("Masukkan Umur = "))
+print(Umur_saya, "bertipe", type(Umur_saya))
+IPK = float(input("Masukkan IPK = "))
+print(IPK, "bertipe", type(IPK))
+Menikah = bool(input("Masukkan Menikah = "))
+print(Menikah, "bertipe", type(Menikah))
