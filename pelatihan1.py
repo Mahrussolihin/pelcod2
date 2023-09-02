19fed4c6248700584fd64237488deda0cb665135
Mahrussolihin <143859887+Mahrussolihin@users.noreply.github.com>
2023-09-02 13:24:52 +0700

    Add files via upload
    UPLOAD FILE TERBARU

--- pelatihan1.py
+++ pelatihan1.py
@@ -14,6 +14,7 @@ Matkul_yang_diambil3 = {"SO", "Etika Profesi"}
 print("Matkul lainnya : ", Matkul_yang_diambil3, "bertipe", type(Matkul_yang_diambil3))
 
 print()
+
 print(33 + 5)
 print(33 - 5)
 print(33 * 5)
@@ -21,13 +22,13 @@ print(33 / 5)
 print(33 // 5)
 print(33 % 5)
 
+
 print()
 
 Nama_saya = str(input("Masukkan Nama = " ))
-print(Nama_saya,"bertipe", type(Nama_saya))
 Umur_saya = int(input("Masukkan Umur = "))
-print(Umur_saya, "bertipe", type(Umur_saya))
 IPK = float(input("Masukkan IPK = "))
-print(IPK, "bertipe", type(IPK))
 Menikah = bool(input("Masukkan Menikah = "))
-print(Menikah, "bertipe", type(Menikah))
+Matkul_yang_diambil = list(input("Masukkan Matkul1 = "))
+Matkul_yang_diambil = tuple(input("Masukkan Matkul2 = "))
+Matkul_yang_diambil = set(input("Masukkan Matkul3 = "))
